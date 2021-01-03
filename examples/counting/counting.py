a9a2a1acc84ddc184d60412c8974f8f7c421d918
timoerb <timoer.buckinx@ixor.be>
2021-01-03 22:55:19 +0100

    Added examples

---
+++ examples/counting/counting.py
@@ -0,0 +1,7 @@
+from time import sleep
+i=0
+while True:
+	print(i)
+	i+=1
+	sleep(3)
+
---
+++ examples/loopy/loop.py
@@ -0,0 +1,8 @@
+import datetime
+from time import sleep
+
+while True:
+	print("Date is: "+str(datetime.datetime.now()))
+	sleep(5)
+
+
